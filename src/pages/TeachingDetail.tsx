680e0782eff2bb889a5932fa645a397630e4ed05
dev <costantine.dev@gmail.com>
2026-02-06 10:59:02 +0300

    bold keyword

--- src/pages/TeachingDetail.tsx
+++ src/pages/TeachingDetail.tsx
@@ -20,17 +20,30 @@ const TeachingDetail: React.FC = () => {
   const boldKeywords = (text: string) => {
     if (!settings.highlightKeywords) return text;
 
-    const keywords = ['God', 'Christ', 
-      'Jesus', 'Holy Spirit', 'Bible', 
-      'Scripture', 'Three Angle\'s Message',
-       'Redemption', 'Eternal Life', 'Mungu', 
-       'Roho', 'Yesu', 'Ufunuo', 'Heaven', 
-       'Father', 'Son','uasi wa sheria',
-      'the transgression of the law',
-    'The Seventh Plague and the Special Resurrection',
-    'God Announces the Time of Christ’s Coming',
-    
-    ];
+    const keywords = ['Only Two Classes', 'Family Members Are Separated', 'No Excuse for Willful Blindness', 'The Importance of Practical Benevolence','The Terror of the Lost',
+      'Jesus', 'Holy Spirit', 'Bible', 'Sunday-Law Advocates Do Not Realize What They Are Doing', 'Rome Will Regain Her Lost Supremacy', 'Motive Gives Character to Actions',
+      'Scripture', 'Three Angle\'s Message', 'Not to Sit in Quietude, Doing Nothing', 'A National Sunday Law Means National Apostasy', 'What the Seal of God Is','Homeward Bound',
+      'Redemption', 'Eternal Life', 'Mungu', 'Oppose Sunday Laws by Pen and Vote', 'National Apostasy Will Be Followed by National Ruin','Enforcement of Sunday Observance Is the Test',
+      'Roho', 'Yesu', 'Ufunuo', 'Heaven', 'The United States Will Pass a Sunday Law', 'Universal Sunday Legislation', 'A Likeness to Christ in Character','The Wicked Are Slain',
+      'Father', 'Son', 'uasi wa sheria', 'Arguments Used by Sunday-Law Advocates', 'The Whole World Will Support Sunday Legislation','Awake Ye That Sleep and Arise','The Angels Sing, Christ Has Conquered',
+      'the transgression of the law', 'Protestantism and Catholicism Act in Concert', 'The Controversy Centers in Christendom','When the Mark of the Beast Is Received',
+      'The Seventh Plague and the Special Resurrection', 'Sunday Laws Honor Rome', 'Show No Defiance', 'Beauty of Truth Made Apparent by Opposition','From Caves and Dens and Dungeons',
+      'God Announces the Time of Christ’s Coming', 'Refrain From Work on Sunday', 'Engage in Spiritual Activities on Sunday','Descends in Power and Glory','From Ocean Depths and Mines and Mountains',
+      'The Sabbath the Great Point at Issue', 'We Ought to Obey God Rather Than Men', 'In the Sealing Time Now', 'Oh, That God’s Seal May Be Placed Upon Us!','The Saints Given Crowns and Harps',
+      'The Sunday-Law Movement in the 1880s', 'Judged by the Light We Have Received','What the Mark of the Beast Is','The Reaction of Those Who Pierced Him','Destruction of the Wicked an Act of Mercy'
+      ,'Pigo la Saba na Ufufuo wa Kipekee','Anatangaza Wakati wa Kuja kwa Kristo','Hofu ya Waliopotea','Anashuka kwa Uwezo na Utukufu','Mwitikio wa Wale Waliomchoma','Amkeni, Ninyi Mnaolala, na Msimame'
+    ,'Kutoka Katika Mapango na Mashimo na Magereza','Kutoka Vilindi vya Bahari na Migodi na Milima','Waovu Wanauawa','Uharibifu wa Waovu ni Tendo la Rehema','Safari ya Kuelekea Nyumbani',
+    'Malaika Wanaimba, Kristo Ameshinda','Watakatifu Wanapewa Taji na Vinubi','Chapa ya Mnyama ni Nini','Wakati Ambapo Chapa ya Mnyama Inapokelewa','Utekelezaji wa Ushikaji wa Jumapili ndilo Jaribio'
+   ,'Makundi Mawili Tu','Wanafamilia Wanatenganishwa','Kuhukumiwa kwa Nuru Tuliyoipokea','Hakuna Udhibiti kwa Upofu wa Makusudi','Umuhimu wa Hisani ya Matendo','Nia Hutoa Tabia kwa Matendo','Muhuri wa','ni Nini',
+   'Mfano wa Kristo katika Tabia','Katika Wakati wa Kupigwa Muhuri Sasa','Sabato Ndilo Suala Kuu la Mgogoro','Vuguvugu la Sheria ya Jumapili katika Miaka ya 1880','Watetezi wa Sheria ya Jumapili Hawatambui Wanachokifanya',
+   ,'Kutoketi katika Utulivu, Bila Kufanya Lolote','Kupinga Sheria za Jumapili kwa Kalamu na Kura','Marekani Itapitisha Sheria ya Jumapili','Hoja Zinazotumiwa na Watetezi wa Sheria ya Jumapili',
+   'Sheria za Jumapili Huheshimu Rumi','Sheria ya Kitaifa ya Jumapili Inamaanisha Uasi wa Kitaifa','Uasi wa Kitaifa Utafuatiwa na Angamizo la Kitaifa','Sheria ya Jumapili ya Ulimwengu Mzima',
+   'Ulimwengu Wote Utategemeza Sheria ya Jumapili','Mgogoro Unajikita katika Ukristo','Usionyeshe Ukaidi','Jiepushe na Kazi Siku ya Jumapili','Jihusishe na Shughuli za Kiroho Siku ya Jumapili','Uzuri wa Ukweli Hudhihirishwa na Upinzani',
+   'Kuliko Wanadamu','Imetupasa Kumtii'
+  
+  
+  
+  ];
     let boldedText = text;
     keywords.forEach(keyword => {
       const regex = new RegExp(`\\b${keyword}\\b`, 'gi');
